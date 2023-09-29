1ae25ca347cffa3bbc2c6980fa11e59883a6adef
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2023-09-28 19:45:09 -1000

    test for connection

--- uh-white-pag/resources/js/app.js
+++ uh-white-pag/resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import ComponentA from './components/ComponentA.vue';
+
+const app = createApp({});
+
+app.component('ComponentA', ComponentA);
+
+app.mount("#app");
